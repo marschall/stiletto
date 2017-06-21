daa8073d50695ac444891697ecb4842edeecf787
Philippe Marschall <philippe.marschall@gmail.com>
2017-06-21 19:53:16 +0200

    bump processor

--- stiletto-processor/src/main/java/com/github/marschall/stiletto/processor/Processor.java
+++ stiletto-processor/src/main/java/com/github/marschall/stiletto/processor/Processor.java
@@ -1,23 +1,52 @@
 package com.github.marschall.stiletto.processor;
 
+import static com.github.marschall.stiletto.processor.Processor.ANNOTATION_NAME;
 import static javax.lang.model.SourceVersion.RELEASE_8;
 
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 
-@SupportedAnnotationTypes("com.github.marschall.stiletto.api.generation.GenerateAspects")
+@SupportedAnnotationTypes(ANNOTATION_NAME)
 @SupportedSourceVersion(RELEASE_8)
 public class Processor extends AbstractProcessor {
 
-    @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        // TODO Auto-generated method stub
-        return false;
+  static final String ANNOTATION_NAME = "com.github.marschall.stiletto.api.generation.GenerateAspects";
+
+  private ProcessingEnvironment processingEnv;
+
+  @Override
+  public synchronized void init(ProcessingEnvironment processingEnv) {
+    super.init(processingEnv);
+    this.processingEnv = processingEnv;
+  }
+
+  @Override
+  public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+    TypeElement generateAspects = this.processingEnv.getElementUtils().getTypeElement(ANNOTATION_NAME);
+    ExecutableElement generateAspectsValues = null;
+    for (Element member : generateAspects.getEnclosedElements()) {
+      if (member.getKind() == ElementKind.METHOD && member.getSimpleName().contentEquals("value")) {
+        generateAspectsValues = (ExecutableElement) member;
+      }
     }
+    for (Element processedClass : roundEnv.getElementsAnnotatedWith(generateAspects)) {
+      for (AnnotationMirror annotation : processedClass.getAnnotationMirrors()) {
+        AnnotationValue annotationValue = annotation.getElementValues().get(generateAspectsValues);
+//        annotationValue.
+      }
+    }
+    return true;
+  }
 
 }
