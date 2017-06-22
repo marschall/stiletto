9ed8d01c907b4a4ebc407d0d6569fbec1cfbffee
Philippe Marschall <philippe.marschall@gmail.com>
2017-06-22 10:33:13 +0200

    processor does not explode

--- stiletto-demo/src/main/java/com/github/marschall/stiletto/demo/aspect/TransactionAspect2.java
+++ stiletto-demo/src/main/java/com/github/marschall/stiletto/demo/aspect/TransactionAspect2.java
@@ -1,5 +1,9 @@
 package com.github.marschall.stiletto.demo.aspect;
 
+import java.lang.annotation.Annotation;
+
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.github.marschall.stiletto.api.injection.DeclaredAnnotation;
@@ -7,6 +11,76 @@ import com.github.marschall.stiletto.api.injection.DeclaredAnnotation;
 public class TransactionAspect2 {
 
   public void inTransaction(@DeclaredAnnotation Transactional transactional) {
+
+  }
+
+  static class X implements Transactional {
+
+    @Override
+    public Class<? extends Annotation> annotationType() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    @Override
+    public String value() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    @Override
+    public String transactionManager() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    @Override
+    public Propagation propagation() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    @Override
+    public Isolation isolation() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    @Override
+    public int timeout() {
+      // TODO Auto-generated method stub
+      return 0;
+    }
+
+    @Override
+    public boolean readOnly() {
+      // TODO Auto-generated method stub
+      return false;
+    }
+
+    @Override
+    public Class<? extends Throwable>[] rollbackFor() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    @Override
+    public String[] rollbackForClassName() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    @Override
+    public Class<? extends Throwable>[] noRollbackFor() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    @Override
+    public String[] noRollbackForClassName() {
+      // TODO Auto-generated method stub
+      return null;
+    }
 
   }
 
--- stiletto-processor/src/main/java/com/github/marschall/stiletto/processor/Processor.java
+++ stiletto-processor/src/main/java/com/github/marschall/stiletto/processor/Processor.java
@@ -1,6 +1,7 @@
 package com.github.marschall.stiletto.processor;
 
-import static com.github.marschall.stiletto.processor.Processor.ANNOTATION_NAME;
+import static com.github.marschall.stiletto.processor.Processor.GENERATE_ASPECTS;
+import static com.github.marschall.stiletto.processor.Processor.GENERATE_ASPECT;
 import static javax.lang.model.SourceVersion.RELEASE_8;
 
 import java.util.Set;
@@ -17,11 +18,13 @@ import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 
-@SupportedAnnotationTypes(ANNOTATION_NAME)
+@SupportedAnnotationTypes({GENERATE_ASPECT, GENERATE_ASPECTS})
 @SupportedSourceVersion(RELEASE_8)
 public class Processor extends AbstractProcessor {
 
-  static final String ANNOTATION_NAME = "com.github.marschall.stiletto.api.generation.GenerateAspects";
+  static final String GENERATE_ASPECTS = "com.github.marschall.stiletto.api.generation.GenerateAspects";
+
+  static final String GENERATE_ASPECT = "com.github.marschall.stiletto.api.generation.GenerateAspect";
 
   private ProcessingEnvironment processingEnv;
 
@@ -33,7 +36,8 @@ public class Processor extends AbstractProcessor {
 
   @Override
   public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-    TypeElement generateAspects = this.processingEnv.getElementUtils().getTypeElement(ANNOTATION_NAME);
+    TypeElement generateAspects = this.processingEnv.getElementUtils().getTypeElement(GENERATE_ASPECTS);
+    TypeElement generateAspect = this.processingEnv.getElementUtils().getTypeElement(GENERATE_ASPECT);
     ExecutableElement generateAspectsValues = null;
     for (Element member : generateAspects.getEnclosedElements()) {
       if (member.getKind() == ElementKind.METHOD && member.getSimpleName().contentEquals("value")) {
@@ -41,6 +45,12 @@ public class Processor extends AbstractProcessor {
       }
     }
     for (Element processedClass : roundEnv.getElementsAnnotatedWith(generateAspects)) {
+      for (AnnotationMirror annotation : processedClass.getAnnotationMirrors()) {
+        AnnotationValue annotationValue = annotation.getElementValues().get(generateAspectsValues);
+//        annotationValue.
+      }
+    }
+    for (Element processedClass : roundEnv.getElementsAnnotatedWith(generateAspect)) {
       for (AnnotationMirror annotation : processedClass.getAnnotationMirrors()) {
         AnnotationValue annotationValue = annotation.getElementValues().get(generateAspectsValues);
 //        annotationValue.
