219cdabce68b54c2c968588d9cf019734e28914e
Philippe Marschall <philippe.marschall@gmail.com>
2017-07-02 16:02:18 +0200

    implement more tests

--- stiletto-tests/src/test/java/com/github/marschall/stiletto/tests/generic/AdvisedGenericInterfaceTest.java
+++ stiletto-tests/src/test/java/com/github/marschall/stiletto/tests/generic/AdvisedGenericInterfaceTest.java
@@ -1,14 +1,34 @@
 package com.github.marschall.stiletto.tests.generic;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.github.marschall.stiletto.tests.BeforeCountingAspect;
 
 public class AdvisedGenericInterfaceTest {
 
+  private BeforeCountingAspect aspect;
+
+  @Before
+  public void setUp() {
+    this.aspect = new BeforeCountingAspect();
+  }
+
   @Test
   public void test() {
-    fail("Not yet implemented");
+
+    AdvisedGenericInterface<String> targetObject = () ->  {
+      assertEquals(1, this.aspect.getInvocationCount());
+      return "ok";
+    };
+
+    AdvisedGenericInterface<String> proxy = new AdvisedGenericInterface_<>(targetObject, this.aspect);
+
+    assertEquals(0, this.aspect.getInvocationCount());
+    assertEquals("ok", proxy.genericMethod());
+    assertEquals(1, this.aspect.getInvocationCount());
   }
 
 }
--- stiletto-tests/src/test/java/com/github/marschall/stiletto/tests/generic/AdvisedNonGenericServiceTest.java
+++ stiletto-tests/src/test/java/com/github/marschall/stiletto/tests/generic/AdvisedNonGenericServiceTest.java
@@ -1,14 +1,31 @@
 package com.github.marschall.stiletto.tests.generic;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.github.marschall.stiletto.tests.BeforeCountingAspect;
 
 public class AdvisedNonGenericServiceTest {
 
+  private BeforeCountingAspect aspect;
+
+  @Before
+  public void setUp() {
+    this.aspect = new BeforeCountingAspect();
+  }
+
   @Test
   public void test() {
-    fail("Not yet implemented");
+
+    AdvisedNonGenericService targetObject = new AdvisedNonGenericService();
+
+    AdvisedNonGenericService proxy = new AdvisedNonGenericService_(targetObject, this.aspect);
+
+    assertEquals(0, this.aspect.getInvocationCount());
+    assertEquals("ok", proxy.genericMethod());
+    assertEquals(1, this.aspect.getInvocationCount());
   }
 
 }
