82f219634342401c09cb380eba1f150bbe676b4d
Philippe Marschall <philippe.marschall@gmail.com>
2017-07-02 17:32:57 +0200

    replace method name with el

--- stiletto-demo/src/main/java/com/github/marschall/stiletto/demo/aspect/LoggingAspect.java
+++ stiletto-demo/src/main/java/com/github/marschall/stiletto/demo/aspect/LoggingAspect.java
@@ -2,17 +2,17 @@ package com.github.marschall.stiletto.demo.aspect;
 
 import com.github.marschall.stiletto.api.advice.AfterReturning;
 import com.github.marschall.stiletto.api.advice.Before;
-import com.github.marschall.stiletto.api.injection.MethodName;
+import com.github.marschall.stiletto.api.injection.Evaluate;
 
 public class LoggingAspect {
 
   @Before
-  public void logEntering(@MethodName String methodName) {
+  public void logEntering(@Evaluate("${joinpoint.methodName}") String methodName) {
     System.out.println("entering " + methodName);
   }
 
   @AfterReturning
-  public void logExiting(@MethodName String methodName) {
+  public void logExiting(@Evaluate("${joinpoint.methodName}") String methodName) {
     System.out.println("exiting " + methodName);
   }
 
