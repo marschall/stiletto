26c1a35fcfef7c0b7555833011023e90e7bb0e4b
Philippe Marschall <philippe.marschall@gmail.com>
2017-07-30 21:00:10 +0200

    add mh demo

---
+++ stiletto-demo/src/test/java/com/github/marschall/stiletto/demo/proxy/MethodHandlesTest.java
@@ -0,0 +1,33 @@
+package com.github.marschall.stiletto.demo.proxy;
+
+import static org.junit.Assert.assertTrue;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+import org.junit.Test;
+
+public class MethodHandlesTest {
+
+  private static final MethodHandle PRIVATE_METHOD;
+
+  static {
+    try {
+      PRIVATE_METHOD = MethodHandles.lookup()
+        .findSpecial(MethodHandlesTest.class, "privateMethod", MethodType.methodType(Void.TYPE), MethodHandlesTest.class);
+    } catch (ReflectiveOperationException e) {
+      throw new IllegalStateException("could not find method", e);
+    }
+  }
+
+  @Test
+  public void callMethodHandle() throws Throwable {
+    PRIVATE_METHOD.invokeExact(this);
+  }
+
+  private void privateMethod() {
+    assertTrue(true);
+  }
+
+}
